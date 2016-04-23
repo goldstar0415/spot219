16f03f15727cf0e1a41e403d3fe9f604c1c46b51
Turkish Spot <codeaway.petar@yahoo.com>
2016-04-23 13:21:30 +0000

    log in tweaks

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,6 +1,6 @@
 class Place < ActiveRecord::Base
   validates :name, presence: true, length: { minimum: 3, maximum: 25 }
-  validates :about, presence: true, length: { minimum: 10, maximum: 400 }
+  validates :about, presence: true, length: { minimum: 10, maximum: 500 }
   validates :country, presence: true
   validates :city, presence: true
   validates :phone, presence: true
