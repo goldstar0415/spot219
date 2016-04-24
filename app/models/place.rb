f4730e1c6289198dd726fa6ac3a7579c42fc89e8
Turkish Spot <codeaway.petar@yahoo.com>
2016-04-24 18:00:27 +0000

    add upload-image feature to places and fixed alignments in show place

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,7 @@
 class Place < ActiveRecord::Base
   belongs_to :user
+  has_attached_file :image, styles: { medium: "640x426>", thumb: "200x134#" }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
   validates :name, presence: true, length: { minimum: 3, maximum: 25 }
   validates :about, presence: true, length: { minimum: 10, maximum: 500 }
   validates :country, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160423182351) do
+ActiveRecord::Schema.define(version: 20160424171925) do
 
   create_table "places", force: :cascade do |t|
     t.string   "name"
@@ -28,6 +28,10 @@ ActiveRecord::Schema.define(version: 20160423182351) do
     t.datetime "updated_at"
     t.integer  "user_id"
     t.string   "map"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
