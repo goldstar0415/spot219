d7161d75a25ece81cba27ff053990318480ab0e1
JackyVo <jackyinvn@gmail.com>
2016-05-10 11:54:54 +0700

    Config mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,4 +1,7 @@
 class UserMailer < ApplicationMailer
+  FROM_MAILER = 'trangtrinhaxinh.vn@gmail.com'
+  default from: FROM_MAILER
+
   def notify email, noti
     @body = noti.body
     mail to: email, subject: noti.title
